cfe9a30bccffa32ceda2c4f2e19fe3664c3c83dc
Wassem Darkwa <waseemdarkwa@gmail.com>
2022-09-27 18:16:25 +0100

    .

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -6,25 +6,25 @@ import {
   Paper,
   TextField,
   Typography,
-} from "@mui/material";
+} from '@mui/material';
 
-import React from "react";
-import { useDispatch } from "react-redux";
-import { login } from "../../action/auth";
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../../action/auth';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
   const paperStyle = {
     padding: 20,
-    fontFamily: "Nunito",
-    fontWeight: "700",
-    height: "70vh",
+    fontFamily: 'Nunito',
+    fontWeight: '700',
+    height: '70vh',
     width: 400,
-    margin: "60px auto",
+    margin: '60px auto',
   };
   const dispatch = useDispatch();
   let history = useHistory();
-  const loginData = { username: "", password: "" };
+  const loginData = { email: '', password: '' };
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(login(loginData, history));
@@ -33,61 +33,48 @@ const Login = () => {
   return (
     <Grid>
       <Paper style={paperStyle} elevation={10}>
-        <Grid item align="center">
+        <Grid item align='center'>
           <Typography
-            variant="h5"
-            color="primary"
-            sx={{ fontFamily: "Nunito", fontWeight: "700" }}
+            variant='h5'
+            color='primary'
+            sx={{ fontFamily: 'Nunito', fontWeight: '700' }}
           >
             StuLancer
           </Typography>
         </Grid>
         <form onSubmit={handleSubmit}>
           <TextField
-            label="Email/Username"
-            placeholder="Enter email/username"
-            variant="standard"
+            label='Email'
+            placeholder='Enter email'
+            variant='standard'
             required
             fullWidth
-            helperText="Enter first name first"
-            sx={{ marginTop: 4, marginBottom: 2, fontFamily: "Nunito" }}
-            onChange={(e) => (loginData.username = e.target.value)}
+            helperText='Enter your email'
+            sx={{ marginTop: 4, marginBottom: 2, fontFamily: 'Nunito' }}
+            onChange={(e) => (loginData.email = e.target.value)}
           />
 
           <TextField
-            label="Password"
-            placeholder="Enter password"
-            variant="standard"
+            label='Password'
+            placeholder='Enter password'
+            variant='standard'
             fullWidth
             required
-            type="password"
+            type='password'
             sx={{ marginBottom: 3 }}
             onChange={(e) => (loginData.password = e.target.value)}
           />
 
-          <FormControlLabel
-            control={<Checkbox name="checkedB" color="primary" />}
-            label={
-              <Typography
-                gutterBottom
-                variant="caption"
-                sx={{ fontFamily: "Nunito", fontWeight: "700" }}
-              >
-                Remember Me
-              </Typography>
-            }
-          />
-
           <Button
             sx={{ marginBottom: 1 }}
-            variant="contained"
+            variant='contained'
             fullWidth
-            type="submit"
+            type='submit'
           >
             Sign in
           </Button>
         </form>
-        <Typography sx={{ fontFamily: "Nunito" }} variant="caption">
+        <Typography sx={{ fontFamily: 'Nunito' }} variant='caption'>
           By joining I agree to receive emails from StuLancer.
         </Typography>
       </Paper>
