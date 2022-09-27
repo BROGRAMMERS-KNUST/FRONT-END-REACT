52ac389272ed9edd85e078f24497330367641b12
edwin ofori <oforiedwin9@gmail.com>
2022-09-27 12:07:44 +0000

    started about us page

---
+++ src/Pages/AboutUS.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Box, Link, Breadcrumbs, Typography } from "@mui/material";
+import StulancersAvailable from "../components/StulancersAvailable";
+import Navbar from "../components/Header/Navbar";
+
+function AboutUsPage(){
+ return (
+         <Box sx={{ 
+                    bgcolor: 'primary.main',
+                    color: 'white',
+                    width: '100px',
+                    height: '100px',
+                    padding: '15px',
+                    '&:hover': {
+                                bgcolor: 'primary.light',
+                               },
+                  }}
+              >
+                
+         </Box>
+
+        )   
+}
