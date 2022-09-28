95a1c1b133cad27b79bd150e7cd532fce8978a4c
Albert Asare-Appiah <ahlbhertojunior@gmail.com>
2022-09-28 18:21:28 +0000

    fixed webdev page

--- src/Pages/WebDevelopment.js
+++ src/Pages/WebDevelopment.js
@@ -2,17 +2,9 @@ import { Typography, Box, Breadcrumbs, Link, Button } from "@mui/material";
 import React from "react";
 import Navbar from "../components/Header/Navbar";
 import WebDev from "../components/PageCards/WebDev";
-import StulancersAvailable from "../components/StulancersAvailable";
-import { useDispatch } from "react-redux";
-import { createPost } from "../action/posts";
+import StulancersAvailable from "./StulancersAvailable";
 
 export default function WebDevelopment() {
-  const data = { name: "Ahl Bherto", reference: 203944949 };
-  const dispatch = useDispatch();
-  const handleClick = (e) => {
-    e.preventDefault();
-    dispatch(createPost(data));
-  };
   return (
     <Box sx={{ marginTop: 10, marginLeft: 4, marginRight: 4 }}>
       <Navbar />
@@ -26,14 +18,6 @@ export default function WebDevelopment() {
         Photography
       </Typography>
       <WebDev />
-      <Button
-        size="large"
-        color="primary"
-        variant="outlined"
-        onClick={handleClick}
-      >
-        Click me
-      </Button>
       <StulancersAvailable />
     </Box>
   );
