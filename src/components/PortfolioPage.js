25b125e85fc6b1cf242cbcf2c87967523ce3f52a
Wassem Darkwa <waseemdarkwa@gmail.com>
2022-10-06 16:11:10 +0100

    Average rating added to StuLancer page

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -1,10 +1,13 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Grid, Paper, Typography } from '@mui/material';
 import React from 'react';
 import PortfolioPic from '../images/images_3.png';
 import { useState } from 'react';
 
+import StarRoundedIcon from '@mui/icons-material/StarRounded';
+import PersonIcon from '@mui/icons-material/Person';
 const PortfolioPage = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+
   return (
     <Box sx={{ minHeight: '78vh', marginLeft: 4, marginRight: 4 }}>
       <Typography variant='h1' fontFamily='Nunito' marginTop={20} gutterBottom>
@@ -18,6 +21,48 @@ const PortfolioPage = () => {
       <Typography variant='h4' fontFamily='Nunito'>
         Welcome to your porfolio page
       </Typography>
+      <Grid container spacing={6} flexWrap>
+        <Grid
+          item
+          //First Grid box
+        >
+          <Box
+            sx={{
+              backgroundColor: '#e14e67',
+              width: 200,
+              borderRadius: 2,
+              boxShadow: 4,
+              marginTop: 6,
+            }}
+          >
+            <StarRoundedIcon
+              sx={{ color: 'white', marginLeft: 1.5, marginTop: 2 }}
+              fontSize='large'
+            />
+            <Typography
+              variant='subtitle1'
+              sx={{ fontFamily: 'Nunito', marginLeft: 1.5, marginTop: 1 }}
+              color='white'
+              gutterBottom
+            >
+              Average Rating
+            </Typography>
+            <Typography
+              variant='h5'
+              component='div'
+              sx={{
+                fontFamily: 'Nunito',
+                fontWeight: '700',
+                marginLeft: 1.5,
+                paddingBottom: 2,
+              }}
+              color='white'
+            >
+              4.5/5
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
