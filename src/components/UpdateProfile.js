fd35197fc328ecd1dc23f1922c7ad05b431df9a8
Wassem Darkwa <waseemdarkwa@gmail.com>
2022-10-14 16:54:00 +0100

    Update Stulancer profile update

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,40 +1,43 @@
-import { Button, Grid, Paper, TextField, Typography } from "@mui/material";
+import { Button, Grid, Paper, TextField, Typography } from '@mui/material';
 
-import React from "react";
-import { updatefreelancerinfo } from "../action/auth";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
-import FileBase from "react-file-base64";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
+import React from 'react';
+import { updatefreelancerinfo } from '../action/auth';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import FileBase from 'react-file-base64';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useState } from 'react';
 const UpdateProfile = () => {
-  const user = JSON.parse(localStorage.getItem("profile"));
+  const user = JSON.parse(localStorage.getItem('profile'));
   const dispatch = useDispatch();
   const history = useHistory();
+  const [disabled, setDisabled] = useState(false);
+
   const freelancerData = {
     fullName: user.result.fullname,
     bio: user.result.bio,
     portfolioLink: user.result.portfolioLink,
     telephoneNumber: user.result.telephoneNumber,
     whatsappLink: user.result.whatsappLink,
-    profilePic: user.result.profilePic,
   };
 
   let freelancerId = null;
   const paperStyle = {
     padding: 20,
-    fontFamily: "Nunito",
-    fontWeight: "700",
-    height: "72vh",
+    fontFamily: 'Nunito',
+    fontWeight: '700',
+    height: '78vh',
     width: 400,
-    margin: "100px auto",
+    margin: '100px auto',
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     freelancerId = user.result._id;
+    setDisabled(true);
     dispatch(updatefreelancerinfo(freelancerData, history, freelancerId));
     console.log(freelancerData);
   };
@@ -42,20 +45,20 @@ const UpdateProfile = () => {
   return (
     <Grid>
       <Paper style={paperStyle} elevation={6}>
-        <Grid item align="center">
+        <Grid item align='center'>
           <Typography
-            variant="h5"
-            color="primary"
-            sx={{ fontFamily: "Nunito", fontWeight: "700", marginBottom: 3 }}
+            variant='h5'
+            color='primary'
+            sx={{ fontFamily: 'Nunito', fontWeight: '700', marginBottom: 3 }}
           >
             Stulancer
           </Typography>
         </Grid>
         <form onSubmit={handleSubmit}>
           <TextField
-            label="Full Name"
-            placeholder="Enter your fullname"
-            variant="standard"
+            label='Full Name'
+            placeholder='Enter your fullname'
+            variant='standard'
             required
             fullWidth
             multiline
@@ -66,9 +69,9 @@ const UpdateProfile = () => {
             }}
           />
           <TextField
-            label="Bio"
-            placeholder="Enter your Bio"
-            variant="standard"
+            label='Bio'
+            placeholder='Enter your Bio'
+            variant='standard'
             required
             fullWidth
             multiline
@@ -80,9 +83,9 @@ const UpdateProfile = () => {
           />
 
           <TextField
-            label="Portfolio Link"
-            placeholder="Enter Portfolio link"
-            variant="standard"
+            label='Portfolio Link'
+            placeholder='Enter Portfolio link'
+            variant='standard'
             required
             fullWidth
             multiline
@@ -93,10 +96,10 @@ const UpdateProfile = () => {
             sx={{ marginBottom: 2 }}
           />
           <TextField
-            label="Telephone Number"
-            type="number"
-            placeholder="Enter Telephone number"
-            variant="standard"
+            label='Telephone Number'
+            type='number'
+            placeholder='Enter Telephone number'
+            variant='standard'
             required
             fullWidth
             defaultValue={user.result.telephoneNumber}
@@ -107,9 +110,9 @@ const UpdateProfile = () => {
             sx={{ marginBottom: 2 }}
           />
           <TextField
-            label="WhatsApp Link"
-            placeholder="Enter WhatsApp Link"
-            variant="standard"
+            label='WhatsApp Link'
+            placeholder='Enter WhatsApp Link'
+            variant='standard'
             fullWidth
             multiline
             defaultValue={user.result.whatsappLink}
@@ -119,47 +122,47 @@ const UpdateProfile = () => {
             sx={{ marginBottom: 2 }}
           />
 
-          <FileBase
-            type="file"
-            multiple={false}
-            onDone={({ base64 }) => {
-              freelancerData.profilePic = base64;
-            }}
-          />
           <FormControl sx={{ marginRight: 2, minWidth: 360, marginBottom: 2 }}>
-            <InputLabel sx={{ marginLeft: -1.5 }} id="service">
+            <InputLabel sx={{ marginLeft: -1.9, marginTop: 1.3 }} id='service'>
               Service
             </InputLabel>
             <Select
-              labelId="service"
-              variant="standard"
-              id="demo-simple-select-autowidth"
+              labelId='service'
+              variant='standard'
+              id='demo-simple-select-autowidth'
               fullWidth
               defaultValue={user.result.profilePic}
               onChange={(e) => {
                 freelancerData.service = e.target.value;
               }}
-              label="service"
+              label='service'
             >
-              <MenuItem value="Photographer">Photographer</MenuItem>
-              <MenuItem value="Web Developer">Web Developer</MenuItem>
-              <MenuItem value="Caterer">Caterer</MenuItem>
-              <MenuItem value="Graphic Designer">Graphic Designer</MenuItem>
-              <MenuItem value="Author">CV Writer</MenuItem>
-              <MenuItem value="Other">Other</MenuItem>
+              <MenuItem value='Photographer'>Photographer</MenuItem>
+              <MenuItem value='Web Developer'>Web Developer</MenuItem>
+              <MenuItem value='Caterer'>Caterer</MenuItem>
+              <MenuItem value='Graphic Designer'>Graphic Designer</MenuItem>
+              <MenuItem value='Author'>CV Writer</MenuItem>
+              <MenuItem value='Other'>Other</MenuItem>
             </Select>
           </FormControl>
-
+          <FileBase
+            type='file'
+            multiple={false}
+            onDone={({ base64 }) => {
+              freelancerData.profilePic = base64;
+            }}
+          />
           <Button
             sx={{
               marginTop: 1.7,
               marginBottom: 1,
-              fontFamily: "Nunito",
-              fontWeight: "700",
+              fontFamily: 'Nunito',
+              fontWeight: '700',
             }}
-            variant="contained"
+            variant='contained'
+            disabled={disabled}
             fullWidth
-            type="submit"
+            type='submit'
           >
             Update profile
           </Button>
