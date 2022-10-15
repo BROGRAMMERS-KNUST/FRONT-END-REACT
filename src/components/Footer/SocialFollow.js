69f57cc57df259fee18026a672e44e72476a052d
Wassem Darkwa <waseemdarkwa@gmail.com>
2022-10-15 11:49:02 +0100

    Footer update

--- src/components/Footer/SocialFollow.js
+++ src/components/Footer/SocialFollow.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   faYoutube,
   faFacebook,
@@ -7,69 +7,69 @@ import {
   faGoogle,
   faWhatsapp,
   faTelegram,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Box, Stack } from "@mui/material";
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Box, Stack } from '@mui/material';
 
 export default function SocialFollow() {
   return (
     <Box marginRight={80}>
-      <div class="social-container">
-        <div class="social-container">
-          <Stack direction="row" spacing={2}>
+      <div class='social-container'>
+        <div class='social-container'>
+          <Stack direction='row' spacing={2}>
             <a
-              href="https://wa.me/242764001"
-              className="whatsApp icon"
-              target="blank"
+              href='https://wa.me/242764001'
+              className='whatsApp icon'
+              target='blank'
             >
-              <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+              <FontAwesomeIcon color='white' icon={faWhatsapp} size='2x' />
             </a>
 
             <a
-              href="https://www.youtube.com/"
-              className="youtube social"
-              target="blank"
+              href='https://www.youtube.com/'
+              className='youtube social'
+              target='blank'
             >
-              <FontAwesomeIcon icon={faYoutube} size="2x" />
+              <FontAwesomeIcon color='white' icon={faYoutube} size='2x' />
             </a>
 
             <a
-              href="https://www.facebook.com/"
-              className="facebook social"
-              target="blank"
+              href='https://www.facebook.com/'
+              className='facebook social'
+              target='blank'
             >
-              <FontAwesomeIcon icon={faFacebook} size="2x" />
+              <FontAwesomeIcon color='white' icon={faFacebook} size='2x' />
             </a>
 
             <a
-              href="https://www.twitter.com/"
-              className="twitter social"
-              target="blank"
+              href='https://www.twitter.com/'
+              className='twitter social'
+              target='blank'
             >
-              <FontAwesomeIcon icon={faTwitter} size="2x" />
+              <FontAwesomeIcon color='white' icon={faTwitter} size='2x' />
             </a>
 
             <a
-              href="https://www.gmail.com/"
-              className="google social"
-              target="blank"
+              href='https://www.gmail.com/'
+              className='google social'
+              target='blank'
             >
-              <FontAwesomeIcon icon={faGoogle} size="2x" />
+              <FontAwesomeIcon color='white' icon={faGoogle} size='2x' />
             </a>
 
             <a
-              href="https://www.instagram.com/"
-              className="instagram social"
-              target="blank"
+              href='https://www.instagram.com/'
+              className='instagram social'
+              target='blank'
             >
-              <FontAwesomeIcon icon={faInstagram} size="2x" />
+              <FontAwesomeIcon color='white' icon={faInstagram} size='2x' />
             </a>
             <a
-              href="https://t.me/0242764001"
-              className="telegram social"
-              target="blank"
+              href='https://t.me/0242764001'
+              className='telegram social'
+              target='blank'
             >
-              <FontAwesomeIcon icon={faTelegram} size="2x" />
+              <FontAwesomeIcon color='white' icon={faTelegram} size='2x' />
             </a>
           </Stack>
         </div>
