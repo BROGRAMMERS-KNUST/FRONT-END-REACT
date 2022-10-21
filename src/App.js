6742916b02a19aa49021315e4137c51bec6e4ef4
Wassem Darkwa <waseemdarkwa@gmail.com>
2022-10-21 20:14:53 +0100

    Minor updates

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 //fonts imports
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import React from "react";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import React from 'react';
 //css files
-import "./App.css";
-import Background from "./components/Main/Background";
-import Main from "./components/Main/Main";
-import Header from "./components/Header/Header";
-import PhotograhyPage from "./Pages/PhotograhyPage";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+import Background from './components/Main/Background';
+import Main from './components/Main/Main';
+import Header from './components/Header/Header';
+import PhotograhyPage from './Pages/PhotograhyPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import GraphicDesignPage from "./Pages/GraphicDesignPage";
-import FoodServicesPage from "./Pages/FoodServicesPage";
-import WritingAndTranslationPage from "./Pages/WritingAndTranslationPage";
-import FreelancerPage from "./Pages/FreelancerPage";
-import FreelancerInfo from "./components/FreelancerInfo";
-import PortfolioPage from "./Pages/PortfolioPage";
-import FooterMain from "./components/Footer/FooterMain";
-import AboutUS from "./Pages/AboutUS";
-import Webby from "./Pages/Webby";
-import UpdateProfile from "./components/UpdateProfile";
-import IndividualRoles from "./components/AboutUs/IndividualRoles";
-import WhyStulancer from "./components/Main/WhyStulancer";
-import LandingCarousel from "./components/Main/LandingCarousel";
-import Background1 from "./components/Main/Background1";
-import Background2 from "./components/Main/Background2";
-import ForgotPassword from "./components/Auth/ForgotPassword";
-import UpdateProfileHirer from "./components/UpdateProfileHirer";
-import OtherInfo from "./components/OtherInfo";
-import Background3 from "./components/Main/Background3";
-import Page404 from "./components/Page404";
-import HowToSell from "./Pages/HowToSell";
+import GraphicDesignPage from './Pages/GraphicDesignPage';
+import FoodServicesPage from './Pages/FoodServicesPage';
+import WritingAndTranslationPage from './Pages/WritingAndTranslationPage';
+import FreelancerPage from './Pages/FreelancerPage';
+import FreelancerInfo from './components/FreelancerInfo';
+import PortfolioPage from './Pages/PortfolioPage';
+import FooterMain from './components/Footer/FooterMain';
+import AboutUS from './Pages/AboutUS';
+import Webby from './Pages/Webby';
+import UpdateProfile from './components/UpdateProfile';
+import IndividualRoles from './components/AboutUs/IndividualRoles';
+import WhyStulancer from './components/Main/WhyStulancer';
+import LandingCarousel from './components/Main/LandingCarousel';
+import Background1 from './components/Main/Background1';
+import Background2 from './components/Main/Background2';
+import ForgotPassword from './components/Auth/ForgotPassword';
+import UpdateProfileHirer from './components/UpdateProfileHirer';
+import OtherInfo from './components/OtherInfo';
+import Background3 from './components/Main/Background3';
+import Page404 from './components/Page404';
 
 function App() {
   return (
@@ -41,62 +40,59 @@ function App() {
         <div>
           <Header />
           <Switch>
-            <Route exact path="/">
+            <Route exact path='/'>
               <LandingCarousel />
               <Background1 />
               <Main />
               <Background3 />
               <Background2 />
             </Route>
-            <Route path="/photography">
+            <Route path='/photography'>
               <PhotograhyPage />
             </Route>
-            <Route path="/graphic-design">
+            <Route path='/graphic-design'>
               <GraphicDesignPage />
             </Route>
-            <Route path="/webdevelopment">
+            <Route path='/webdevelopment'>
               <Webby />
             </Route>
-            <Route path="/food-services">
+            <Route path='/food-services'>
               <FoodServicesPage />
             </Route>
-            <Route path="/writing&translation">
+            <Route path='/writing&translation'>
               <WritingAndTranslationPage />
             </Route>
-            <Route path="/freelancerpage">
+            <Route path='/freelancerpage'>
               <FreelancerPage />
             </Route>
-            <Route path="/freelancerinfo">
+            <Route path='/freelancerinfo'>
               <FreelancerInfo />
             </Route>
-            <Route path="/portfoliopage">
+            <Route path='/portfoliopage'>
               <PortfolioPage />
             </Route>
-            <Route path="/about-us">
+            <Route path='/about-us'>
               <AboutUS />
             </Route>
-            <Route exact path="/about-us_individual-roles">
+            <Route exact path='/about-us_individual-roles'>
               <IndividualRoles />
             </Route>
 
-            <Route path="/updateprofile">
+            <Route path='/updateprofile'>
               <UpdateProfile />
             </Route>
-            <Route path="/passwordrecovery/forgot-password">
+            <Route path='/passwordrecovery/forgot-password'>
               <ForgotPassword />
             </Route>
-            <Route path="/updateprofilehirer">
+            <Route path='/updateprofilehirer'>
               <UpdateProfileHirer />
             </Route>
 
-            <Route path="/otherinfo">
+            <Route path='/otherinfo'>
               <OtherInfo />
             </Route>
-            <Route path="/howtosell">
-              <HowToSell />
-            </Route>
 
-            <Route path="*">
+            <Route path='*'>
               <Page404 />
             </Route>
           </Switch>
