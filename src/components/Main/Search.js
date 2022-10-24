475d8b4068d5a1165e354c366f785c34e60fdc88
Wassem Darkwa <waseemdarkwa@gmail.com>
2022-10-24 11:45:45 +0100

    Search functionality development

--- src/components/Main/Search.js
+++ src/components/Main/Search.js
@@ -1,14 +1,55 @@
 import { Box, Paper } from '@mui/material';
 import React from 'react';
-
+import { useHistory } from 'react-router-dom';
 import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
+import { photography } from './SearchArrays';
 const Search = () => {
+  const history = useHistory();
+  const [searchInput, setsearchInput] = useState('');
+
+  const checkPhotgraphy = (array, data) => {
+    const splittedData = data.split(' ');
+    console.log(splittedData);
+
+    return array.some((arrayData) => {
+      for (let i = 0; i < splittedData.length; i++) {
+        if (splittedData[i] === arrayData) {
+          return splittedData[i] === arrayData;
+        }
+      }
+    });
+
+    // return array.some((arrayData) => {
+    //   return splittedData[0] === arrayData;
+    // });
+
+    // // splittedData.forEach((element) => {
+    // //   console.log(`Element = ${element}`);
+    // //   console.log(element === '2');
+    // });
+    // array.some((arrayData) => {
+    //   arrayData.forEach((element) => {
+    //     data === element;
+    //   });
+    // });
+  };
+  const handleSearchInput = (e) => {
+    setsearchInput(e.target.value);
+  };
+  const handleSearch = () => {
+    console.log(searchInput);
+
+    console.log(checkPhotgraphy(photography, searchInput));
+    if (checkPhotgraphy(photography, searchInput)) {
+      history.push('/photography');
+    }
+  };
   return (
     <Box>
       <Paper
-        component='form'
         sx={{
           p: '2px 4px',
           display: 'flex',
@@ -31,9 +72,9 @@ const Search = () => {
             fontSize: 22,
           }}
           placeholder='Type in a project idea.....'
-          inputProps={{ 'aria-label': 'search google maps' }}
+          onChange={handleSearchInput}
         />
-        <IconButton sx={{ p: '10px' }} aria-label='search'>
+        <IconButton sx={{ p: '10px' }} onClick={handleSearch}>
           <SearchIcon sx={{ color: 'grey' }} />
         </IconButton>
       </Paper>
