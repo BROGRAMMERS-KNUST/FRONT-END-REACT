eb98619225847d0d520a489be63cc7e5919afa5d
Wassem Darkwa <waseemdarkwa@gmail.com>
2022-10-27 19:28:12 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "./components/theme";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './components/theme';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
 import {
   legacy_createStore as createStore,
   applyMiddleware,
   compose,
-} from "redux";
-import thunk from "redux-thunk";
-import reducers from "./reducers";
+} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 root.render(
   <React.StrictMode>
